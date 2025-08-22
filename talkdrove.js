c423cd0297a332d32ae01772644f59380434d598
a-rizak <abdiriiadan095@gmail.com>
2025-08-22 16:29:10 +0300

    Create talkdrove.js

---
+++ talkdrove.js
@@ -0,0 +1,25 @@
+{
+  "bot-name": "ARIZAK-MD",
+  "description": "ᴊᴀᴠᴀsᴄʀɪᴘᴛ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴀʀɪᴢᴀᴋ",
+  "logo": "https://files.catbox.moe/juxfce.jpg",
+  "keywords": ["bot", "whatsapp", "multi-device"],
+  "documentation-link": "https://arizakmd-pairsession.onrender.com/",
+  "owner-verification": "abdiriiadan095@gmail.com",
+  "env": {
+    "SESSION_ID": {
+      "description": "Put your SESSION_ID here. It starts with arizak~",
+      "value": "",
+      "required": true
+    },
+    "OWNER_NUMBER": {
+      "description": "Enter owner number",
+      "value": "254700000000",
+      "required": true
+    },
+    "TIMEZONE": {
+      "description": "Enter country timezone",
+      "value": "Africa/Nairobi",
+      "required": true
+    }
+  }
+    }
